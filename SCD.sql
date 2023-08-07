978e82180e09c7fc691d50bd9173b7e99671b7b8
nalgasibiur <nicolas.algasibiur@gmail.com>
2023-08-06 22:08:05 -0300

    Define the structure of the script

---
+++ SCD.sql
@@ -0,0 +1,8 @@
+-- Type 0.
+-- Do nothing to keep updating the dimension table.
+
+-- Type 1.
+-- Update all fields in the table, except the PK field.
+
+-- Type 2.
+-- Add a new version of the record, adding fields like StartDate, EndDate and Current.
